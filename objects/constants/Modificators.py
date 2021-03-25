608e3b010cc13c45fcf7d6d205fa3758ed12222a
Mike Shenin <i@kotrik.ru>
2021-03-25 21:33:38 +0300

    add: Mirror Modificators.py

--- objects/constants/Modificators.py
+++ objects/constants/Modificators.py
@@ -36,10 +36,11 @@ class Mods(IntFlag):
     Key1 = 1 << 26
     Key3 = 1 << 27
     Key2 = 1 << 28
-    LastMod = 1 << 29
+    LastMod = 1 << 29  # can be named as ScoreV2 in last osu versions
+    Mirror = 1 << 30
     KeyMod = Key1 | Key2 | Key3 | Key4 | Key5 | Key6 | Key7 | Key8 | Key9 | KeyCoop
 
-    FreeModAllowed = Hidden | HardRock | DoubleTime | Flashlight | FadeIn | Easy | Relax | Relax2 | SpunOut | NoFail | Easy | HalfTime | Autoplay | KeyMod
+    FreeModAllowed = Hidden | HardRock | DoubleTime | Flashlight | FadeIn | Easy | Relax | Relax2 | SpunOut | NoFail | Easy | HalfTime | Autoplay | KeyMod | Mirror
     ScoreIncreaseMods = Hidden | HardRock | DoubleTime | Flashlight | FadeIn | Easy | Relax | Relax2 | SpunOut | NoFail | Easy | HalfTime | Autoplay | SuddenDeath | Perfect | KeyMod | Target | Random | Nightcore | LastMod
     SpeedAltering = DoubleTime | Nightcore | HalfTime
 
